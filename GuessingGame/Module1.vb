57662b4cbb54ce205071d16d7fab817b165a16d5
Udayakantha <igudaya@yahoo.com>
2014-04-12 14:30:52 +0530

    vb.net console

---
+++ GuessingGame/Module1.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim ChkValid = True
+
+        While ChkValid
+
+            Dim GN As String = Console.ReadLine()
+
+            If IsNumeric(GN) Then
+
+                If GN <= 100 And GN > 0 Then
+                    If GN = 42 Then
+                        ChkValid = False
+                        Console.WriteLine("Correct")
+                    Else
+                        ChkValid = True
+                        Console.WriteLine("Incorrect")
+                    End If
+                Else
+                    ChkValid = True
+                    Console.WriteLine("Wrong Input")
+                End If
+            Else
+                ChkValid = True
+                Console.WriteLine("Wrong Input")
+            End If
+
+            'If Not ChkValid Then
+            '    Console.WriteLine("Correct")
+            'Else
+            '    Console.WriteLine("Incorrect")
+            'End If
+
+        End While
+
+
+    End Sub
+
+End Module
